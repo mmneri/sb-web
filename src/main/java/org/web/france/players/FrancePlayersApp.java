6ba85bba977948c9c6655033b1388ca630e89617
Marouen MNERI <mmneri@santeclair.fr>
2018-10-10 15:29:33 +0200

    update

--- src/main/java/org/web/france/players/FrancePlayersApp.java
+++ src/main/java/org/web/france/players/FrancePlayersApp.java
@@ -3,16 +3,16 @@ package org.web.france.players;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class FrancePlayersApp extends SpringBootServletInitializer {
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(FrancePlayersApp.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(FrancePlayersApp.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(FrancePlayersApp.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(FrancePlayersApp.class, args);
+    }
 }
--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,13 +1,13 @@
 
 $(document).ready(function () {
     $.ajax({
-        url: 'http://localhost:8181/fff/players',
+        url: 'http://localhost/fff/players',
         type: 'GET',
         dataType: 'json',
         success: function (result) {
             var insert = '';
             $.each(result, function (index, item) {
-                insert += '<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"><div class="course-item"><div class="course-img"><img src="' + item.photoURL + '" alt="" width="100%"></div><div class="course-body"><div class="course-desc"><h4 class="course-title"><a href="#">' + item.nom + '</a></h4></div></div><div class="course-footer"><div class="course-seats">&nbsp;</div><div class="course-button"><a href="#">VOIR PLUS</a></div></div></div></div>';
+                insert += '<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12"><div class="course-item"><div class="course-img"><img src="' + item.photoURL + '" alt="" width="100%"></div><div class="course-body"><div class="course-desc"><h4 class="course-title"><a href="#">' + item.nom + '</a></h4><p>' + item.poste + '</p></div></div><div class="course-footer"><div class="course-seats">&nbsp;</div><div class="course-button"><a href="#">VOIR PLUS</a></div></div></div></div>';
             });
             $('#players').html(insert);
         }
